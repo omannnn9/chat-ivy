cecb8626685b10a84d8e0080249b76b0d9728d1a
omannnn9 <dilloooman26@gmail.com>
2025-06-04 22:51:56 +0400

    Delete static/GLTFLoader.js

--- static/GLTFLoader.js
+++
@@ -1,6 +0,0 @@
-// static/GLTFLoader.js (used as a bridge for loading via module)
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.149.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.149.0/examples/jsm/loaders/GLTFLoader.js';
-
-window.THREE = window.THREE || {};
-window.THREE.GLTFLoader = GLTFLoader;
